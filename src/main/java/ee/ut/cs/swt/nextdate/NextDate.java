7681f739f0b96a69a961bcdb38a1ad336236b858
SiimSaaresalu <siim.saaresalu@ut.ee>
2024-04-02 21:56:25 +0300

    Completed task 4

--- src/main/java/ee/ut/cs/swt/nextdate/NextDate.java
+++ src/main/java/ee/ut/cs/swt/nextdate/NextDate.java
@@ -100,8 +100,7 @@ public class NextDate {
 						return "Invalid Input Date";
 				}
 				//
-				else if(day > 29) //invalid input as February will never have more than 29 days
-					return "Invalid Input Date";
+				else return "Invalid Input Date";
 			}
 		}
 		//return the string representing the nextDate, in the form MM/DD/YY
